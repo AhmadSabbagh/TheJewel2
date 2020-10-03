71de71534e2df120b8f5045815d2148f363421e9
ahmad <ahmad_Sabbagh7@hotmail.com>
2020-10-03 21:30:23 +0100

    Test 1 by Ahmad

---
+++ app/src/main/java/com/am/engsabbagh/thejewel/test1.java
@@ -0,0 +1,15 @@
+package com.am.engsabbagh.thejewel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class test1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test1);
+        // This acctivity has been created by AHMAD
+    }
+}
