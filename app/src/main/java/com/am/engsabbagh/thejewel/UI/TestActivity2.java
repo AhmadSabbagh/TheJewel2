731664ada36945bd2a668248b1f8d91b30507bde
RIYAD <eng.riyadalali93@gmail.com>
2020-10-03 23:44:17 +0300

    test 2

---
+++ app/src/main/java/com/am/engsabbagh/thejewel/UI/TestActivity2.java
@@ -0,0 +1,16 @@
+package com.am.engsabbagh.thejewel.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.am.engsabbagh.thejewel.R;
+
+public class TestActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test2);
+    }
+}
