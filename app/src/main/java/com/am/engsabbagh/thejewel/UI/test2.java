21bd4b6f710839df8ca8ba26eb70dbef24a07859
ahmad <ahmad_Sabbagh7@hotmail.com>
2020-10-03 22:52:31 +0100

    Test 2 by Ahmad

---
+++ app/src/main/java/com/am/engsabbagh/thejewel/UI/test2.java
@@ -0,0 +1,17 @@
+package com.am.engsabbagh.thejewel.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.am.engsabbagh.thejewel.R;
+
+public class test2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test2);
+        // Created by Ahmad
+    }
+}
