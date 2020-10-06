1025f1267838228b9b1f2a35444e776d939e9e24
RIYAD <eng.riyadalali93@gmail.com>
2020-10-06 09:10:24 +0300

    Added Splash Screen 1- lanuching Splash screen for 5 seconds 2- after StartActivity.java runs for 5 seconds it will move to PrivacyPolicy ativity

--- app/src/main/java/com/am/engsabbagh/thejewel/UI/StartActivity.java
+++ app/src/main/java/com/am/engsabbagh/thejewel/UI/StartActivity.java
@@ -3,15 +3,29 @@ package com.am.engsabbagh.thejewel.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.am.engsabbagh.thejewel.R;
 
 public class StartActivity extends AppCompatActivity {
 
+    /*  Duration of  Waiting  */
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(StartActivity.this, PrivacyPolicy.class);
+                StartActivity.this.startActivity(mainIntent);
+                StartActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
-}
+    }
