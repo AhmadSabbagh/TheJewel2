b220a273b256ddc0eb015981f4bef7aa359114f6
RIYAD <eng.riyadalali93@gmail.com>
2020-10-06 09:00:16 +0300

    1- Adding StartActivity.java for app starting layout 2- fixing the error of min sdk is 24 of binding view 3- changing build gradle file to min sdk to 24

---
+++ app/src/main/java/com/am/engsabbagh/thejewel/UI/StartActivity.java
@@ -0,0 +1,17 @@
+
+package com.am.engsabbagh.thejewel.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.am.engsabbagh.thejewel.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
