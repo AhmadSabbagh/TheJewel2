4ced908c071d0bef5b6285d10a3b3beaf3172ace
devengriyadalali <dev.engriyadalali@gmail.com>
2020-10-13 21:01:34 +0300

    1- Load Language saved from shared preference 2- if there is no saved language go to LanguageOptionAvtivity 3- load language of app based on the language saved

--- app/src/main/java/com/am/engsabbagh/thejewel/UI/LanguageOptionAvtivity.java
+++ app/src/main/java/com/am/engsabbagh/thejewel/UI/LanguageOptionAvtivity.java
@@ -8,6 +8,7 @@ import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,15 +21,20 @@ import java.util.Locale;
 public class LanguageOptionAvtivity extends AppCompatActivity {
     Button SelectLanguage,Arabic,English;
     AttolSharedPreference attolSharedPreference=new AttolSharedPreference(this); // getting this context
+    String LanguageLoaded="";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language_option_avtivity);
+
+        LanguageLoaded=attolSharedPreference.getKey("languageSelected"); // load the language that is saved
+        LanguageSaved(); // check language
         SelectLanguage=(Button)findViewById(R.id.selectlanguage); // select language button
         Arabic=(Button)findViewById(R.id.arabic);  //arabic language
         English=(Button)findViewById(R.id.english);  //english language
+
         Arabic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,32 +53,58 @@ public class LanguageOptionAvtivity extends AppCompatActivity {
 
     //-> Set language english and save it to locale
     private void SetLanguageEnglish() {
-        attolSharedPreference.setKey("language","en");
+        attolSharedPreference.setKey("languageSelected","en");
         Locale myLocale = new Locale("en");
         Resources res = getResources();
         DisplayMetrics dm = res.getDisplayMetrics();
         Configuration conf = res.getConfiguration();
         conf.locale = myLocale;
         res.updateConfiguration(conf, dm);
-        Intent refresh = new Intent(this, LanguageOptionAvtivity.class);
-        finish();
-        startActivity(refresh);
+        Intent refresh = new Intent(this, PrivacyPolicy.class);
+       finish();
+       startActivity(refresh);
         Toast.makeText(this, "English Selected", Toast.LENGTH_SHORT).show();
 
     }
 
     //-> set language in locale phone to english
     private void SetLanguageArabic() {
-        attolSharedPreference.setKey("language","ar");
+        attolSharedPreference.setKey("languageSelected","ar");
         Locale myLocale = new Locale("ar");
         Resources res = getResources();
         DisplayMetrics dm = res.getDisplayMetrics();
         Configuration conf = res.getConfiguration();
         conf.locale = myLocale;
         res.updateConfiguration(conf, dm);
-        Intent refresh = new Intent(this, LanguageOptionAvtivity.class);
+       Intent refresh = new Intent(this, PrivacyPolicy.class);
         finish();
         startActivity(refresh);
         Toast.makeText(this, "تم اختيار العربية", Toast.LENGTH_SHORT).show();
     }
+    //----------------------------------------------------------------------------------------------
+    //-> this method make sure that user is already selected language
+    public void LanguageSaved()
+    {
+        if (LanguageLoaded!=null) {
+            if (LanguageLoaded.equals("ar")) {
+                SetLanguageArabic(); //  set language of app to be arabic based on shard Preference
+                Intent intent = new Intent(LanguageOptionAvtivity.this, PrivacyPolicy.class);
+                LanguageOptionAvtivity.this.startActivity(intent);
+                Log.i("Language", LanguageLoaded.toString());
+            } else if (LanguageLoaded.equals("en")) {
+                SetLanguageEnglish(); //  set language of app to be English based on shared preference
+                Intent intent = new Intent(LanguageOptionAvtivity.this, PrivacyPolicy.class);
+                LanguageOptionAvtivity.this.startActivity(intent);
+
+                Log.i("Language", LanguageLoaded.toString());
+            } else {
+                Intent intent = new Intent(LanguageOptionAvtivity.this, LanguageOptionAvtivity.class);
+                LanguageOptionAvtivity.this.startActivity(intent);
+                LanguageOptionAvtivity.this.finish();
+                Log.i("Language", LanguageLoaded.toString());
+            }
+        }
+
+    }
+    //----------------------------------------------------------------------------------------------
    }//end Class
--- app/src/main/java/com/am/engsabbagh/thejewel/UI/StartActivity.java
+++ app/src/main/java/com/am/engsabbagh/thejewel/UI/StartActivity.java
@@ -6,10 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
+import com.am.engsabbagh.thejewel.HelperClass.AttolSharedPreference;
 import com.am.engsabbagh.thejewel.R;
 
 public class StartActivity extends AppCompatActivity {
+    AttolSharedPreference attolSharedPreference=new AttolSharedPreference(this);
+    String LanguageSaved="";
 
     /*  Duration of  Waiting  */
     private final int SPLASH_DISPLAY_LENGTH = 5000;
@@ -22,10 +26,13 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(StartActivity.this, LanguageOptionAvtivity.class);
+               Intent mainIntent = new Intent(StartActivity.this, LanguageOptionAvtivity.class);
                 StartActivity.this.startActivity(mainIntent);
                 StartActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
-    }
-    }
+
+        //---------------------Test if Language is already Saved------------------------------------
+
+    } // end onCreate Activity
+    }  //end Class
