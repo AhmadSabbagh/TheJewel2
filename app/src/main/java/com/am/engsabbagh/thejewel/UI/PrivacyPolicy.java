50cfb02ce3ad68782dc3961c949e8fcc5322aa38
devengriyadalali <dev.engriyadalali@gmail.com>
2020-10-14 08:32:37 +0300

    Privacy Policy Activity Finished -> if user accept privacy policy go to country selection -> if user already accepted privacy policy go to login activity

--- app/src/main/java/com/am/engsabbagh/thejewel/UI/PrivacyPolicy.java
+++ app/src/main/java/com/am/engsabbagh/thejewel/UI/PrivacyPolicy.java
@@ -2,15 +2,51 @@ package com.am.engsabbagh.thejewel.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
+import com.am.engsabbagh.thejewel.CountryChooser;
+import com.am.engsabbagh.thejewel.HelperClass.AttolSharedPreference;
 import com.am.engsabbagh.thejewel.R;
 
 public class PrivacyPolicy extends AppCompatActivity {
+    AttolSharedPreference attolSharedPreference=new AttolSharedPreference(this);
+    CheckBox checkBox;
+    String PrivacyPolicyState="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
-    }
-}
+        PrivacyPolicy();
+        checkBox=(CheckBox)findViewById(R.id.checkboxagree);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            //-> If the user agree the privacy policy of app we must go to select country
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked==true)
+                {
+                    attolSharedPreference.setKey("PrivacyPolicy","Accepted"); // save this to phone
+                    Intent intent=new Intent(PrivacyPolicy.this, CountryChooser.class); // go to country chooser
+                    PrivacyPolicy.this.startActivity(intent);
+                    PrivacyPolicy.this.finish();// finish this activity
+                }
+            }
+        });// end checkbox listner
+    }//end onCreate
+    //----------------------------------------------------------------------------------------------
+    //-> PrivacyPolicyState Check
+    public void PrivacyPolicy() {
+        PrivacyPolicyState=attolSharedPreference.getKey("PrivacyPolicy");
+        if (PrivacyPolicyState != null) {
+       if (PrivacyPolicyState.equals("Accepted")) {
+                Intent intent = new Intent(this, LoginActivity.class); // the user is already accepted privacy policy
+                this.startActivity(intent);
+                this.finish();
+            }
+        }
+    }  //----------------------------------------------------------------------------------------------
+}// end Class
