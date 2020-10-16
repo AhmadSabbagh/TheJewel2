04666bbeb9a776651f663adf16927d1db9e50755
devengriyadalali <dev.engriyadalali@gmail.com>
2020-10-16 18:12:58 +0300

    this update includes: 1- added privacy poilcy as text file. 2- select country done. 3- working on fixing layout files.

--- app/src/main/java/com/am/engsabbagh/thejewel/UI/CountryChooser.java
+++ app/src/main/java/com/am/engsabbagh/thejewel/UI/CountryChooser.java
@@ -2,15 +2,65 @@ package com.am.engsabbagh.thejewel.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.am.engsabbagh.thejewel.HelperClass.AttolSharedPreference;
 import com.am.engsabbagh.thejewel.R;
 
 public class CountryChooser extends AppCompatActivity {
+    AttolSharedPreference attolSharedPreference=new AttolSharedPreference(this);
+    Button uae,SaudiArabic;
+    String CountrySelectionState="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country_chooser);
+        Country(); // check if country already selected
+        uae=(Button)findViewById(R.id.uae);  // uae country selection
+        SaudiArabic=(Button)findViewById(R.id.saudiarabia); //  saudiarabic country selection
+        uae.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attolSharedPreference.setKey("Country","UAE");  //saving to phone uae
+                Intent intent=new Intent(CountryChooser.this,LoginActivity.class);
+                CountryChooser.this.startActivity(intent);
+                CountryChooser.this.finish();
+            }
+        }); //end uae selection
+        SaudiArabic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attolSharedPreference.setKey("Country","SaudiArabic"); // saving to phone saudi arabic
+                Intent intent=new Intent(CountryChooser.this,LoginActivity.class);
+                CountryChooser.this.startActivity(intent);
+                CountryChooser.this.finish();
+            }
+        });//end saudi arabic button
+    }// end onCreate Method
+    //----------------------------------------------------------------------------------------------
+    //-> Country Selection Check
+    /*
+    if country is selected go then to login activity
+    else display country selection
+    */
+    public void Country() {
+        CountrySelectionState = attolSharedPreference.getKey("Country");
+        if (CountrySelectionState != null) {
+            if (CountrySelectionState.equals("UAE")) {
+                Intent intent = new Intent(this, LoginActivity.class); // the user is already accepted privacy policy
+                this.startActivity(intent);
+                this.finish();
+            }
+            if (CountrySelectionState.equals("SaudiArabic")) {
+                Intent intent = new Intent(this, LoginActivity.class); // the user is already accepted privacy policy
+                this.startActivity(intent);
+                this.finish();
+            }
+        }
     }
-}
+//--------------------------------------------------------------------------------------------------
+}//end class
--- app/src/main/java/com/am/engsabbagh/thejewel/UI/PrivacyPolicy.java
+++ app/src/main/java/com/am/engsabbagh/thejewel/UI/PrivacyPolicy.java
@@ -6,22 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import com.am.engsabbagh.thejewel.HelperClass.AttolSharedPreference;
 import com.am.engsabbagh.thejewel.R;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class PrivacyPolicy extends AppCompatActivity {
     AttolSharedPreference attolSharedPreference=new AttolSharedPreference(this);
     CheckBox checkBox;
     String PrivacyPolicyState="";
+    String Language="";
+    TextView PrivacyPolicyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
-        PrivacyPolicy();
+        PrivacyPolicyTextView=(TextView)findViewById(R.id.privacy_policy_text);
         checkBox=(CheckBox)findViewById(R.id.checkboxagree);
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        PrivacyPolicyState=attolSharedPreference.getKey("PrivacyPolicy");
+        Language=attolSharedPreference.getKey("languageSelected");
+        try
+        {
+            ReadTextFile();
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        PrivacyPolicy();
+    checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             //-> If the user agree the privacy policy of app we must go to select country
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -35,13 +55,37 @@ public class PrivacyPolicy extends AppCompatActivity {
             }
         });// end checkbox listner
     }//end onCreate
-    //----------------------------------------------------------------------------------------------
+//--------------------------Reading Privacy Policy--------------------------------------------------
+           public void ReadTextFile() throws IOException {
+            String string = "";
+            StringBuilder stringBuilder = new StringBuilder();
+            InputStream is;
+           // if (Language!=null) {
+                if (Language.equals("ar")) {
+                    is = this.getResources().openRawResource(R.raw.privacypolicy_ar);
+                } else {
+                    is = this.getResources().openRawResource(R.raw.privacypolicy_en);
+                }
+        //    }//end if null
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            while (true) {
+                try {
+                    if ((string = reader.readLine()) == null) break;
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                stringBuilder.append(string).append("\n");
+                PrivacyPolicyTextView.setText(stringBuilder);
+            }
+            is.close();
+        }
+       //----------------------------------------------------------------------------------------------
     //-> PrivacyPolicyState Check
     public void PrivacyPolicy() {
-        PrivacyPolicyState=attolSharedPreference.getKey("PrivacyPolicy");
         if (PrivacyPolicyState != null) {
        if (PrivacyPolicyState.equals("Accepted")) {
-                Intent intent = new Intent(this, LoginActivity.class); // the user is already accepted privacy policy
+                Intent intent = new Intent(this, CountryChooser.class); // the user is already accepted privacy policy
                 this.startActivity(intent);
                 this.finish();
             }
