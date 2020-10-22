d8c65c22cc5268869183e87e863894d537490de6
devengriyadalali <dev.engriyadalali@gmail.com>
2020-10-22 21:44:54 +0300

    Start Activity Fixed: 1- Fixing center Logo in screen. 2- Fixing Background of activity

--- app/src/main/java/com/am/engsabbagh/thejewel/UI/ForgetPassword.java
+++ app/src/main/java/com/am/engsabbagh/thejewel/UI/ForgetPassword.java
@@ -3,14 +3,20 @@ package com.am.engsabbagh.thejewel.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.am.engsabbagh.thejewel.R;
 
 public class ForgetPassword extends AppCompatActivity {
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
     }
 }
